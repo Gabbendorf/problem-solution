47c8d496f7ba6721114743603b0059c28be7e94d
Gabba <gabriellamds@yahoo.it>
2021-02-16 15:59:30 +0100

    Delete unused error class

--- lib/errors/messages.rb
+++ lib/errors/messages.rb
@@ -2,5 +2,4 @@
 
 module Messages
   WRONG_COMMAND_ERROR = "Invalid command: make sure you enter all the arguments and a valid format type\n(e.g. ruby combine.rb --format json articles.csv authors.json journals.csv)"
-  WRONG_RESOURCES_ERROR = "Invalid resources: resources must be articles.csv, authors.json and journals.csv in any order"
 end
--- lib/errors/wrong_resources_error.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class WrongResourcesError < StandardError
-  def initialize(message)
-    super(message)
-  end
-end
